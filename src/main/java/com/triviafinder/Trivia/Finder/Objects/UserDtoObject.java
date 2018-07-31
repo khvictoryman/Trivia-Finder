17c61a276ce1fb2dc25f817aa8b488202bd9dea2
khvictoryman <khvictoryman@gmail.com>
2018-07-31 12:01:46 -0500

    User Login/Wireframes
    In this commit I just started imiplementing a tutorial for a user registration page. I have also added some wireframes.

---
+++ src/main/java/com/triviafinder/Trivia/Finder/Objects/UserDtoObject.java
@@ -0,0 +1,97 @@
+package com.triviafinder.Trivia.Finder.Objects;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+import javax.validation.constraints.NotNull;
+
+public class UserDtoObject {
+    @NotNull
+    @NotEmpty
+    private String firstName;
+
+    @NotNull
+    @NotEmpty
+    private String lastName;
+
+    @NotNull
+    @NotEmpty
+    private String username;
+
+    @NotNull
+    @NotEmpty
+    private String password;
+    private String matchingPassword;
+
+    @NotNull
+    @NotEmpty
+    private String email;
+
+    @NotNull
+    @NotEmpty
+    private Integer zipcode;
+
+
+    // standard getters and setters
+
+    @NotNull
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(@NotNull String firstName) {
+        this.firstName = firstName;
+    }
+
+    @NotNull
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(@NotNull String lastName) {
+        this.lastName = lastName;
+    }
+
+    @NotNull
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(@NotNull String username) {
+        this.username = username;
+    }
+
+    @NotNull
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(@NotNull String password) {
+        this.password = password;
+    }
+
+    public String getMatchingPassword() {
+        return matchingPassword;
+    }
+
+    public void setMatchingPassword(String matchingPassword) {
+        this.matchingPassword = matchingPassword;
+    }
+
+    @NotNull
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(@NotNull String email) {
+        this.email = email;
+    }
+
+    @NotNull
+    public Integer getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(@NotNull Integer zipcode) {
+        this.zipcode = zipcode;
+    }
+}
--- src/main/java/com/triviafinder/Trivia/Finder/controllers/UserRegistrationController.java
+++ src/main/java/com/triviafinder/Trivia/Finder/controllers/UserRegistrationController.java
@@ -1,12 +1,19 @@
 package com.triviafinder.Trivia.Finder.controllers;
 
+import com.triviafinder.Trivia.Finder.Objects.UserDtoObject;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.context.request.WebRequest;
 
 @Controller
 public class UserRegistrationController {
-    @RequestMapping(value="UserRegistration")
-    public String UserRegistration() {
-        return "User Registration";
+    @RequestMapping(value = "/user/user_registration", method = RequestMethod.GET)
+    public String showRegistrationForm(WebRequest request, Model model) {
+        UserDtoObject userDto = new UserDtoObject();
+        model.addAttribute("user", userDto);
+        return "user/user_registration";
     }
+
 }
