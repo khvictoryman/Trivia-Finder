d1464b0febe06a6fc15c676ebbd7a7d6a29d4a9f
khvictoryman <khvictoryman@gmail.com>
2018-08-06 17:17:20 -0500

    Pre-Rework of the Host Registration Page
    Just dropping a checkpoint right before I rework the Host Registration page

--- src/main/java/com/triviafinder/Trivia/Finder/controllers/HostProfileController.java
+++ src/main/java/com/triviafinder/Trivia/Finder/controllers/HostProfileController.java
@@ -2,10 +2,11 @@ package com.triviafinder.Trivia.Finder.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HostProfileController {
-    @RequestMapping(value="/hostprofile")
+    @RequestMapping(value="/hostprofile", method = RequestMethod.GET)
     public String hostprofile() {
         return "hostprofile";
     }
